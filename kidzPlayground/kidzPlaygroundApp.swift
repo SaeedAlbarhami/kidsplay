1ad2997d709b8dc3266f1c5bfb4e81ccf3469e9d
emailsoft <emailspaghetti2020@gmail.com>
2023-08-05 22:02:19 +0400

    Initial Commit

---
+++ kidzPlayground/kidzPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  kidzPlaygroundApp.swift
+//  kidzPlayground
+//
+//  Created by Saeed Thabit on 05/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct kidzPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
